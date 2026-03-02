a36d3b857503f6384d344e8d8da4d16a02001593
r0984209 <seppe.dehouwer@student.kuleuven.be>
2026-03-02 16:01:42 +0100

    implementatie bruteforce algo !! WERKT NIET CORRECT !!

---
+++ bruteforce.jl
@@ -0,0 +1,16 @@
+using TensorToolbox
+using LinearAlgebra
+
+function bruteforce(X::AbstractArray, A::Vector{<:AbstractMatrix})
+    d = ndims(X)
+
+    K = A[d]
+    for k in (d-1):-1:1
+        K = kron(K, A[k])
+    end
+
+    y = K * vec(X)
+
+    outsz = ntuple(i -> size(A[i], 1), d)
+    return reshape(y, outsz)
+end
